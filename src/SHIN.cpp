278ea4d8e190fb354f49ed4539fa52f3a4cfdec2
at04 <at04@at04-Macmini>
2019-03-02 15:36:24 +0900

    3.2

--- src/SHIN.cpp
+++ src/SHIN.cpp
@@ -2,7 +2,33 @@
 using namespace std;
 
 
+int a[100001][2];
+int d[100001][2];
+
+
 int main(void)
 {
+	int t;
+	cin>>t;
 
+	while(t--)
+	{
+		int n;
+		cin>>n;
+
+		for(int i=1; i<=n; i++)
+		{
+			cin>>a[i][1];
+		}
+
+		for(int i=1; i<=n; i++)
+		{
+			cin>>a[i][2];
+		}
+
+		for(int i=1; i<=n; i++)
+		{
+			d[i][0] = max(d[i-1][0], d[i-1][1], d[i-1][2]);
+		}
+	}
 }
