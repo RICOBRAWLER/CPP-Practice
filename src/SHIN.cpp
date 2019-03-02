40b48db1d2eeb7a8627f33c7ab46a0b4291d161c
at04 <at04@at04-Macmini>
2019-03-02 16:49:52 +0900

    last

--- src/SHIN.cpp
+++ src/SHIN.cpp
@@ -1,29 +1,14 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 
-int a[10001];
-int d[10001][2];
+int a[10] = {30, 40, 50, 80};
 
 
 int main(void)
 {
-	int n;
-	cin>>n;
-
-	for(int i=1; i<=n; i++)
-	{
-		cin>>a[i];
-	}
-
-	for(int i=1; i<=n; i++)
-	{
-		d[i][0] = max(d[i-1][0], max(d[i-1][1], d[i-1][2]));
-		d[i][1] = d[i-1][0] + a[i];
-		d[i][2] = d[i-1][1] + a[i];
-	}
-
-	int ans = max(d[n][0], max(d[n][1], d[n][2]));
+	int ans = *max_element(a, a+4);
 
 	cout<<ans;
 }
