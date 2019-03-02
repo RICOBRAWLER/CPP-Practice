8cfb887476a2df27a6ea74f93fb9bdc639765701
at04 <at04@at04-Macmini>
2019-03-02 16:29:36 +0900

    wow

--- src/SHIN.cpp
+++ src/SHIN.cpp
@@ -2,46 +2,31 @@
 using namespace std;
 
 
-int a[100001][2];
-int d[100001][2];
+int a[10001];
+int d[10001];
 
 
 int main(void)
 {
-	int t;
-	cin>>t;
+	int n;
+	cin>>n;
 
-	while(t--)
+	for(int i=1; i<=n; i++)
 	{
-		int n;
-		cin>>n;
-
-		for(int i=1; i<=n; i++)
-		{
-			cin>>a[i][1];
-		}
-
-		for(int i=1; i<=n; i++)
-		{
-			cin>>a[i][2];
-		}
-
-		for(int i=1; i<=n; i++)
-		{
-			d[i][0] = max(d[i-1][0], max(d[i-1][1], d[i-1][2]));
-			d[i][1] = max(d[i-1][0], d[i-1][2]) + a[i][1];
-			d[i][2] = max(d[i-1][0], d[i-1][1]) + a[i][2];
-		}
-
-		int ans = d[0][0];
-		for(int i=1; i<=n; i++)
-		{
-			for(int j=0; i<3; j++)
-			{
-				ans = max(ans, d[i][j]);
-			}
-		}
-
-		cout<< ans;
+		cin>>a[i];
 	}
+
+	d[1] = a[1];
+	d[2] = a[1] + a[2];
+
+	for(int i=3; i<=n; i++)
+	{
+		d[i] = d[i-1];
+
+		d[i] = max(d[i], d[i-2] + a[i]);
+
+		d[i] = max(d[i], d[i-3] + a[i-1] + a[i]);
+	}
+
+	cout<<d[n];
 }
