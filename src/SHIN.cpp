80b7ab814783ab837586246bb814749f40cde5cb
at04 <at04@at04-Macmini>
2019-03-09 16:26:36 +0900

    yes

--- src/SHIN.cpp
+++ src/SHIN.cpp
@@ -5,40 +5,52 @@ int a[1000];
 int d[1000];
 int v[1000];
 
-void go(int p) {
-    if (p == -1) {
-        return ;
-    }
-    go(v[p]);
-    cout << a[p] << ' ';
+
+void go(int p)
+{
+	if(p == 0)
+		return;
+	else
+	{
+		go(v[p]);
+		cout<<a[p]<<" ";
+	}
 }
 
-int main() {
-    int n;
-    cin >> n;
-    for (int i=0; i<n; i++) {
-        cin >> a[i];
-    }
-    for (int i=0; i<n; i++) {
-        d[i] = 1;
-        v[i] = -1;
-        for (int j=0; j<i; j++) {
-            if (a[j] < a[i] && d[i] < d[j]+1) {
-                d[i] = d[j]+1;
-                v[i] = j;
-            }
-        }
-    }
-    int ans = d[0];
-    int p = 0;
-    for (int i=0; i<n; i++) {
-        if (ans < d[i]) {
-            ans = d[i];
-            p = i;
-        }
-    }
-    cout << ans << '\n';
-    go(p);
-    cout << '\n';
-    return 0;
+
+int main()
+{
+	int n;
+	cin>>n;
+
+	for(int i=1; i<=n; i++)
+	{
+		cin>>a[i];
+	}
+
+	for(int i=1; i<=n; i++)
+	{
+		d[i] = 1;
+
+		for(int j=1; j<=i; j++)
+		{
+			if(a[i]>a[j] && d[j]+1>d[i])
+			{
+				d[i] = d[j]+1;
+				v[i] = j;
+			}
+		}
+	}
+
+	int ans = d[1];
+	int p = 0;
+
+	for(int i=1; i<=n; i++)
+	{
+		ans = max(ans, d[i]);
+		p = i;
+	}
+
+	cout<<ans<<"\n";
+	go(p);
 }
