9cc0c91fa9f08695a300bd223c91de030e320445
at04 <at04@at04-Macmini>
2019-03-09 16:33:34 +0900

    sfsdf

--- src/SHIN.cpp
+++ src/SHIN.cpp
@@ -3,19 +3,6 @@ using namespace std;
 
 int a[1000];
 int d[1000];
-int v[1000];
-
-
-void go(int p)
-{
-	if(p == 0)
-		return;
-	else
-	{
-		go(v[p]);
-		cout<<a[p]<<" ";
-	}
-}
 
 
 int main()
@@ -30,27 +17,23 @@ int main()
 
 	for(int i=1; i<=n; i++)
 	{
-		d[i] = 1;
+		d[i] = a[i];
 
 		for(int j=1; j<=i; j++)
 		{
-			if(a[i]>a[j] && d[j]+1>d[i])
+			if(a[i]>a[j] && d[j]+a[i]>d[i])
 			{
-				d[i] = d[j]+1;
-				v[i] = j;
+				d[i] = d[j]+a[i];
 			}
 		}
 	}
 
 	int ans = d[1];
-	int p = 0;
 
 	for(int i=1; i<=n; i++)
 	{
 		ans = max(ans, d[i]);
-		p = i;
 	}
 
 	cout<<ans<<"\n";
-	go(p);
 }
